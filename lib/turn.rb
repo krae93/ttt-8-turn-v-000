a2d3dbc8c6ed9436a8144a2651bf6b5c2bda8f93
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-06-30 04:03:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,9 +38,4 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
-  while !index.between?(0, 8)
-    puts "Please enter 1-9:"
-    user_input = gets.strip
-      index = input_to_index(user_input)
-  end
 end
