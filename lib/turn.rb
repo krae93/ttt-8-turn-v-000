ee36a3b9135ecfe33575c7153b6b5ea750fba846
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-06-30 03:36:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,3 +24,10 @@ def position_taken?(board, index)
  #    return false
  #   end
  end
+ def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
