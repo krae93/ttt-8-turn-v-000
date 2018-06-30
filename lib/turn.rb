6833e148de698f5ae51d49e33f7cd626ae0a6ac0
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-06-30 04:05:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,6 +35,7 @@ def move(board, index, char = "X")
 end
 
 def turn(board)
+  while !index.between?(0, 8)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
