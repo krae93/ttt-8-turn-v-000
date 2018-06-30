b36555c71774207fc8b0f157f8a8f7a94c2e0eaa
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-06-30 03:55:51 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,9 +13,11 @@ def position_taken?(board, index)
      return true
    end
  end
+
  def valid_move?(board, index)
     index.between?(0,8) && !position_taken?(board, index)
  end
+
  def display_board(board)
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
    puts "-----------"
@@ -23,6 +25,7 @@ def position_taken?(board, index)
    puts "-----------"
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
+
  def input_to_index(user_input)
   user_input.to_i - 1
 end
